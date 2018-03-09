c5b121331aaaf0cbe0053072172583697b6335b2
donglongyun <316433179@qq.com>
2018-03-09 14:47:43 +0800

    上线了

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -2,10 +2,9 @@
 return array(
     //'配置项'=>'配置值'
     'DB_TYPE' => 'mysql',
-    'DB_DSN' => 'mysql:host=localhost:3307;dbname=marx;charset=utf8',
+    //注意修改端口值及连接地址
+    'DB_DSN' => 'mysql:host=127.0.0.1:3307;dbname=marx;charset=utf8',
     'DB_uSER' => 'root',
-    /*'DB_PWD' => '',*/
-    /*tencent dbrootpwd*/
     'DB_PWD' => '',
     'DB_PREFIX' => 'tp_',
     'DB_PARAMS' => array(
