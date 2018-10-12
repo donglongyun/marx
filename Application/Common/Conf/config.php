faef82bfe029a275f4abb413488ae815ffb8abad
dlink <316433179@qq.com>
2018-10-12 17:30:20 +0800

    Update config.php

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -3,10 +3,10 @@ return array(
     //'配置项'=>'配置值'
     'DB_TYPE' => 'mysql',
     //注意修改端口值及连接地址
-    'DB_DSN' => 'mysql:host=127.0.0.1:3307;dbname=marx;charset=utf8',
-    'DB_uSER' => 'root',
+    'DB_DSN' => 'mysql:host=111.111.111.111:3306;dbname=***;charset=utf8',
+    'DB_uSER' => '',
     'DB_PWD' => '',
-    'DB_PREFIX' => 'tp_',
+    'DB_PREFIX' => '',
     'DB_PARAMS' => array(
         PDO::ATTR_CASE => PDO::CASE_NATURAL
     )
